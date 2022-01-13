890ed3a5f854f5dabce6eb06ed25a02b85bb880a
qwe2733227 <qwe2733227@naver.com>
2022-01-13 12:47:27 +0900

    day10 자바스크립트

---
+++ day10/js/student.js
@@ -0,0 +1,13 @@
+//학색의 학년, 반, 이름 정보를 출력
+var naming = '추신수';            //name이라는 변수 선언 
+var grade = 2; //선언과 동시에 초기화
+// var class; -예약어는 변수로 사용할 수 없다.
+var shcoolClass = 3;  //카멜표기법
+
+
+/* document.write(naming + '<br>');
+document.write(grade + '<br>');
+document.write(shcoolClass + '<br>'); */
+
+document.write('<p><span>'+ naming + "</span> 학생은 " + grade + "학년" 
+    + shcoolClass + " 반입니다.</p>");
