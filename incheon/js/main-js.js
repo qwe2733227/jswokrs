ac8dd1466f927f4e0087708a11d5a944bf755b2c
qwe2733227 <qwe2733227@naver.com>
2022-01-24 11:49:53 +0900

    day17

---
+++ incheon/js/main-js.js
@@ -0,0 +1,26 @@
+//이미지 show
+let picture =["images/bg11.jpg", "images/bg22.jpg", "images/bg55.jpg"]
+        let msg_idx = 0;   //배열의 위치값을 맨 처음 값으로 설정
+
+        showPicture(); //호출
+
+        function showPicture(){
+            let pic = document.getElementById("cup")
+            pic.src = picture[msg_idx];
+            msg_idx++;  //msg_idx += 1
+
+            //다시 0번 인덱스로 설정
+            console.log(picture.length);
+            if(msg_idx === picture.length){  //배열의 크기와 같으면
+                msg_idx = 0;
+            }
+            setTimeout(showPicture, 2000); //콜백 함수(함수의 매개변수로 함수를 설정)
+        }
+
+
+//디지털 시계
+setInterval(function(){   //실행함수
+    const now = new Date();
+    let watch =now.toLocaleTimeString(); //시간표시
+    document.querySelector("#display").innerHTML = watch;
+},1000);
